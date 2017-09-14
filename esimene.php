f5cdb834436b1cebbfa5f1e5dd832deb2ba66057
Valeri Valentinov <irelav@users.noreply.github.com>
2017-09-14 10:47:03 +0300

    Lisatud on esimene php jupp

---
+++ esimene.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Valeri koduleht</title>
+</head>
+<body>
+	<h1>Valeri Valentinov, veebiprogrammeerimine</h1>
+	<p>Seda veebilehte uuendatakse vähemalt korra nädalas.</p>
+	<p>See veebileht oli loodud õppimis otstarbeks!</p>
+	<?php
+		echo "Algas PHP õppimine.";
+	?>
+
+</body>
+</html>
